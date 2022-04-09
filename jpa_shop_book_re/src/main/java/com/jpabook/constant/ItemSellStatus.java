b95f06a449ae179c0b74da5350e5c9d49dd551f5
SangHun <shzero211@naver.com>
2022-04-09 15:44:54 +0900

    JPQL테스트,ItemRepository 생성및 설정

---
+++ jpa_shop_book_re/src/main/java/com/jpabook/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.jpabook.constant;
+
+public enum ItemSellStatus {
+SELL,SOLD_OUT
+}
---
+++ jpa_shop_book_re/src/main/java/com/jpabook/entity/Item.java
@@ -0,0 +1,50 @@
+package com.jpabook.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import com.jpabook.constant.ItemSellStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Item {
+	
+@Id
+@Column(name = "item_id")
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
+
+@Column(nullable = false,length = 50)
+private String itemNm;
+
+@Column(nullable = false)
+private int price;
+
+@Column(nullable = false)
+private int stockNumber;
+@Lob
+@Column(nullable = false)
+private String itemDetail;
+
+@Enumerated(EnumType.STRING)
+private ItemSellStatus itemSellStatus;
+
+private LocalDateTime regTime;
+
+private LocalDateTime updateTime;
+}
